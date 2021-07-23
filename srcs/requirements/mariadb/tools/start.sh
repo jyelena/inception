a9881bf7a0fb9c03b5ca6ab9d24195b0be01bbd2
dmikhaylov <d.y.mikhayov@yandex.ru>
2021-07-24 03:46:16 +0400

    поменял одмена на малышку (учётка администратора)

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -3,7 +3,7 @@ if [ ! -f /tmp/done ]; then
 
     echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" | mysql -u root
     echo "GRANT ALL ON *.* TO '$DB_USER' IDENTIFIED BY '$DB_PASSWORD' WITH GRANT OPTION;" | mysql -u root
-    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'SQL_CONF_ROOT_PASS';" | mysql -u root
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';" | mysql -u root
     echo "FLUSH PRIVILEGES;" | mysql -u root
 
     service mysql stop
